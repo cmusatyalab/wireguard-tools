f8e85ab0d5612937c124d24453b359f6ed374690
Alexey Matveev <tippet@yandex.ru>
2023-06-08 08:08:03 +0300

    Add pre post

--- src/wireguard_tools/wireguard_config.py
+++ src/wireguard_tools/wireguard_config.py
@@ -151,6 +151,11 @@ class WireguardConfig:
     search_domains: list[str] = field(factory=list)
     mtu: int | None = field(converter=optional(int), default=None)
 
+    preup: list[str] = field(factory=list)
+    postup: list[str] = field(factory=list)
+    predown: list[str] = field(factory=list)
+    postdown: list[str] = field(factory=list)
+
     # wireguard-android specific extensions
     included_applications: list[str] = field(factory=list)
     excluded_applications: list[str] = field(factory=list)
@@ -231,6 +236,14 @@ class WireguardConfig:
                 self.included_applications.extend(item for item in value.split(", "))
             elif key == "excludedapplications":
                 self.excluded_applications.extend(item for item in value.split(", "))
+            elif key == "preup":
+                self.preup.append(value)
+            elif key == "postup":
+                self.postup.append(value)
+            elif key == "predown":
+                self.predown.append(value)
+            elif key == "postdown":
+                self.postdown.append(value)
 
     def _add_dns_entry(self, item: str) -> None:
         try:
@@ -258,6 +271,11 @@ class WireguardConfig:
             conf.extend([f"Address = {addr}" for addr in self.addresses])
             conf.extend([f"DNS = {addr}" for addr in self.dns_servers])
             conf.extend([f"DNS = {domain}" for domain in self.search_domains])
+
+            conf.extend([f"PreUp = {cmd}" for cmd in self.preup])
+            conf.extend([f"PostUp = {cmd}" for cmd in self.postup])
+            conf.extend([f"PreDown = {cmd}" for cmd in self.predown])
+            conf.extend([f"PostDown = {cmd}" for cmd in self.postdown])
 
             # wireguard-android specific extensions
             if self.included_applications:
