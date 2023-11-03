02435a239539da062c377b3e638a799abf00badb
Rick Dawson <dawsora@gmail.com>
2023-11-03 10:40:41 -0700

    initial base html template

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@
 from nox_poetry import session
 
 
-@session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
+@session(python=["3.12","3.11", "3.10", "3.9", "3.8", "3.7"])
 def tests(session):
     session.install("pytest", ".")
     session.run("pytest")
--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
