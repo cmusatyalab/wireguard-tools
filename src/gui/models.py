a05b5ae06b70f2c8f16d8ec3c6f2ed885ea170c9
Rick Dawson <dawsora@gmail.com>
2023-11-09 11:42:12 -0500

    added peer details

--- src/gui/models.py
+++ src/gui/models.py
@@ -1,6 +1,8 @@
-import app
+from gui import create_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+app = create_app()
 
 # sqlalchemy instance
 db = SQLAlchemy(app)
@@ -18,9 +20,18 @@ class Config(db.Model):
     allowed_ips = db.Column(db.String(50))
     friendly_name = db.Column(db.String(50))
     friendly_json = db.Column(db.String(50))
-    last_handshake = db.Column(db.String(50))
+    last_handshake = db.Column(db.Float)
     rx_bytes = db.Column(db.Integer)
     tx_bytes = db.Column(db.Integer)
+    description = db.Column(db.Text)
+
+
+class Peer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    config = db.Column(db.String(50))
+    network = db.Column(db.Integer)
+    description = db.Column(db.Text)
 
 
 # JSON Schema
@@ -38,17 +49,53 @@ class ConfigSchema(ma.Schema):
             "friendly_json",
             "last_handshake",
             "rx_bytes",
-            "tx_bytes"
+            "tx_bytes",
+            "description",
         )
 
 
-config_schema =ConfigSchema()
+class PeerSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "config", "network", "description")
+
+
+config_schema = ConfigSchema()
 configs_schema = ConfigSchema(many=True)
+peer_schema = PeerSchema()
+peers_schema = PeerSchema(many=True)
 
 
 if __name__ == "__main__":
     db.create_all()
-    user1 = Config(name="Paul John", email="pj@gmail.com", password="pjmaxson2020#")
-    user2 = Config(name="John Doe", email="JD@gmail.com", password="jdmaxson2020#")
-    db.session.add_all([user1, user2])
+    conf1 = Config(
+        **{
+            "interface": {
+                "address": "10.10.10.11/32",
+                "listern_port": "51820",
+                "private_key": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+            },
+            "peer": {
+                "AllowedIPs": ["0.0.0.0/0", "::/0"],
+                "PublicKey": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+                "PersistentKeepalive": 25,
+                "Endpoint": "myserver.dyndns.org:51820",
+            },
+        }
+    )
+    conf2 = Config(
+        **{
+            "interface": {
+                "address": "10.10.10.17/32",
+                "listern_port": "51820",
+                "private_key": "iISiPlI8UHSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+            },
+            "peer": {
+                "AllowedIPs": ["0.0.0.0/0", "::/0"],
+                "PublicKey": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+                "PersistentKeepalive": 25,
+                "Endpoint": "myserver.dyndns.org:51820",
+            },
+        }
+    )
+    db.session.add_all([conf1, conf2])
     db.session.commit()
--- src/gui/peer.py
+++ src/gui/peer.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 
 # Testing wg input
 import wireguard_tools as wgt
+
 devices = wgt.WireguardDevice.list()
 
 bp = Blueprint("peers", __name__, url_prefix="/peers")
@@ -10,22 +11,44 @@ bp = Blueprint("peers", __name__, url_prefix="/peers")
 peer_list = [
     {
         "name": "peer 1",
-        "interface": {
-            "address": "10.10.10.11/32",
-            "listern_port": "51820",
-            "private_key": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
-        },
-        "peer": {
-            "AllowedIPs": ["0.0.0.0/0", "::/0"],
-            "PublicKey": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
-            "PersistentKeepalive": 25,
-            "Endpoint": "myserver.dyndns.org:51820",
+        "config": {
+            "interface": {
+                "address": "10.10.10.11/32",
+                "listern_port": "51820",
+                "private_key": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+            },
+            "peer": {
+                "AllowedIPs": ["0.0.0.0/0", "::/0"],
+                "PublicKey": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+                "PersistentKeepalive": 25,
+                "Endpoint": "myserver.dyndns.org:51820",
+            },
         },
         "network": 1,
         "description": "description 1",
         "id": 1,
     },
+    {
+        "name": "peer 2",
+        "config": {
+            "interface": {
+                "address": "10.10.10.14/32",
+                "listern_port": "51820",
+                "private_key": "iISiPlI8UHSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+            },
+            "peer": {
+                "AllowedIPs": ["0.0.0.0/0", "::/0"],
+                "PublicKey": "iISiPbGn4wSPhloFOtDN2BgqfJ1MqKKkmm0WtWc9sFE=",
+                "PersistentKeepalive": 25,
+                "Endpoint": "myserver.dyndns.org:51820",
+            },
+        },
+        "network": 1,
+        "description": "description 2",
+        "id": 2,
+    },
 ]
+# print(peer_list)
 
 
 @bp.route("/", methods=["GET", "POST"])
@@ -34,3 +57,13 @@ def peers():
         print("POSTED to peers")
     else:
         return render_template("peers.html", peer_list=peer_list)
+
+
+@bp.route("/<peer_id>", methods=["GET", "POST"])
+def peer(peer_id):
+    if request.method == "POST":
+        print("POSTED to peers")
+    else:
+        peer = next((item for item in peer_list if item["id"] == int(peer_id)), None)
+        print(f"Found: {peer}")
+        return render_template("peer_detail.html", peer=peer)
