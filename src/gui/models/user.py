b591c98b6e08f89ef849588def2868f76f32e776
Rick Dawson <dawsora@gmail.com>
2023-12-07 13:49:38 -0500

    increase character count for password field

--- src/gui/models/user.py
+++ src/gui/models/user.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(128))
+    password = db.Column(db.String(250))
 
 
 # JSON Schema
