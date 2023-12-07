9d448824aa137f639129b203520cfc98024fe34a
Rick Dawson <dawsora@gmail.com>
2023-12-06 22:10:14 -0500

    remove sudo_password from user

--- src/gui/models/user.py
+++ src/gui/models/user.py
@@ -4,20 +4,24 @@ from flask_marshmallow import Marshmallow
 
 ma = Marshmallow()
 
+
 # Create model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)         
+    username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(128))
+
 
 # JSON Schema
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id","name", "email", "password")
+        fields = ("id", "name", "email", "password")
+
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+
 
 def user_load_test_db():
     pass
