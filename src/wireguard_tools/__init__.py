f3284f39f9126223332ace6a38f7e8361d95b0bd
Jan Harkes <jaharkes@cs.cmu.edu>
2024-01-31 23:55:49 -0500

    Bumping to 0.4.5.post.dev0

--- src/wireguard_tools/__init__.py
+++ src/wireguard_tools/__init__.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: MIT
 #
 
-__version__ = "0.4.5"
+__version__ = "0.4.5.post.dev0"
 
 from .wireguard_config import WireguardConfig, WireguardPeer
 from .wireguard_device import WireguardDevice
