143af31789fdaaf9dce1647e1e47c0d18af2d55a
Jan Harkes <jaharkes@cs.cmu.edu>
2024-03-19 12:08:09 -0400

    Add a trivial test to check if the script entrypoint can be imported.

---
+++ tests/test_entrypoints.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024 Carnegie Mellon University
+# SPDX-License-Identifier: MIT
+
+from wireguard_tools.cli import main as _cli_main  # noqa: F401
+
+
+def test_entrypoints():
+    """The real test was if we could import the entrypoints"""
+    assert True
+
