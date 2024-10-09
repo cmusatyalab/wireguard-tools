f7a1ee727dff7bb8e5c8fda7486e810039e2168e
Mark Steward <marksteward@gmail.com>
2024-10-09 17:00:51 +0100

    Split config sections based on entire line
    Allows ignoring of commented-out peers.

--- src/wireguard_tools/wireguard_config.py
+++ src/wireguard_tools/wireguard_config.py
@@ -221,7 +221,7 @@ class WireguardConfig:
     @classmethod
     def from_wgconfig(cls, configfile: TextIO) -> WireguardConfig:
         text = configfile.read()
-        _pre, *parts = re.split(r"\[(Interface|Peer)\]\n", text, flags=re.I)
+        _pre, *parts = re.split(r"^\[(Interface|Peer)\]$", text, flags=re.I | re.M)
         sections = [section.lower() for section in parts[0::2]]
         if sections.count("interface") > 1:
             msg = "More than one [Interface] section in config file"
